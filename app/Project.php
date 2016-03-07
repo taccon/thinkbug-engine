4363e80cd8cd7986ac5ef5f7e7d775e69e32947a
Thomas Bretzke <thomas@taccon.com>
2016-03-07 18:18:41 +0100

    Initial commit.

---
+++ app/Project.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+/**
+ * Class Project
+ *
+ * Model to describe Projects
+ *
+ * @package App
+ * @author Thomas Bretzke <thomas@taccon.com>
+ * @copyright (c) 2016 taccon software
+ */
+class Project extends Model
+{
+    /** @var int $id */
+    protected $id;
+
+    /** @var string $name */
+    protected $name;
+
+    protected $
+}
---
+++ app/Services/ProjectManager.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+/**
+ * Class ProjectManager
+ *
+ * Project service (handles i/o for projects)
+ *
+ * @package App\Services
+ * @author Thomas Bretzke <thomas@taccon.com>
+ * @copyright (c) 2016 taccon software
+ */
+class ProjectManager
+{
+
+}
